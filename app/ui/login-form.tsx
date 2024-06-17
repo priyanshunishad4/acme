23e466e0925c5baf4078c9454c1a6949b3facae2
Priyanshu Nishad <priyanshunishad4@gmail.com>
2024-06-17 21:44:27 +0530

    created new .env file created direction for login

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -18,7 +18,8 @@ export default function LoginForm() {
     <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+          Please log in to continue. Use "user@nextmail.com" as email and
+          "123456" as password
         </h1>
         <div className="w-full">
           <div>
